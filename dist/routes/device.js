c14f1b317572aba829ae9bc18fc8ebe463aa0520
Omer Hadad <omerhadad10@gmail.com>
2022-01-22 23:03:47 +0200

    Change device state function
    Added post request for the api changing the state of a device

--- dist/routes/device.js
+++ dist/routes/device.js
@@ -1,22 +1,42 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 const jsonFunctions_1 = require("../code/jsonFunctions");
 const dataPath = "./resources/data/devices.json";
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     var data = (0, jsonFunctions_1.readJsonFile)(dataPath);
     res.status(200).send({
-        data
+        data,
     });
 });
-router.get('/:name', (req, res) => {
-    var { name } = req.params;
-    var data = (0, jsonFunctions_1.readJsonFile)(dataPath);
-    var devices = data.devices;
+router.get("/:name", (req, res) => {
+    let { name } = req.params;
+    let data = (0, jsonFunctions_1.readJsonFile)(dataPath);
+    let devices = data.devices;
     let deviceData = devices.find((deviceData) => deviceData.name === name);
     res.status(200).send({
-        deviceData
+        deviceData,
+    });
+});
+router.post("/changestatus", (req, res) => {
+    var { deviceName } = req.query;
+    var data = (0, jsonFunctions_1.readJsonFile)(dataPath);
+    for (var i = 0; i < data.devices.length; i++)
+        if (data.devices[i].name == deviceName) {
+            data.devices[i].status = !data.devices[i].status;
+            fs.writeFile(dataPath, JSON.stringify(data), 'utf-8', (error) => {
+                if (error)
+                    return console.log(error);
+            });
+            res.status(200).send({
+                "Messege": `Device named ${deviceName} is now ${data.devices[i].status ? "active" : "not active"}`
+            });
+            return;
+        }
+    res.status(200).send({
+        "Messege": `No device found with name ${deviceName}`
     });
 });
 exports.default = router;
--- src/routes/device.ts
+++ src/routes/device.ts
@@ -1,29 +1,47 @@
-import express = require('express');
+import express = require("express");
+import fs = require("fs");
+import { stringify } from "querystring";
 const router = express.Router();
 
-import { readJsonFile } from '../code/jsonFunctions';
-import { parseToJson } from '../code/jsonFunctions';
+import { readJsonFile } from "../code/jsonFunctions";
+import { parseToJson } from "../code/jsonFunctions";
 
-const dataPath = "./resources/data/devices.json"
+const dataPath = "./resources/data/devices.json";
 
-router.get('/' , (req: express.Request, res: express.Response) => {
-    var data = readJsonFile(dataPath);
-    res.status(200).send({
-        data
-    })
+router.get("/", (req: express.Request, res: express.Response) => {
+  var data = readJsonFile(dataPath);
+  res.status(200).send({
+    data,
+  });
 });
 
-router.get('/:name', (req: express.Request, res: express.Response) => {
-
-    var {name} = req.params;
-    var data = readJsonFile(dataPath);
-    var devices = data.devices;
-    let deviceData = devices.find( (deviceData:any) => deviceData.name === name ) ;
-    res.status(200).send({
-        deviceData
-    })
-    
+router.get("/:name", (req: express.Request, res: express.Response) => {
+  let { name } = req.params;
+  let data = readJsonFile(dataPath);
+  let devices = data.devices;
+  let deviceData = devices.find((deviceData: any) => deviceData.name === name);
+  res.status(200).send({
+    deviceData,
+  });
 });
+router.post("/changestatus", (req: express.Request, res: express.Response) => {
+  var { deviceName } = req.query;
+  var data = readJsonFile(dataPath);
+  for (var i = 0; i < data.devices.length; i++)
+    if (data.devices[i].name == deviceName) {
+      data.devices[i].status = !data.devices[i].status;
+      fs.writeFile(dataPath,JSON.stringify(data),'utf-8',(error) =>{
+          if(error) return console.log(error);
+      } )
 
+      res.status(200).send({
+          "Messege":`Device named ${deviceName} is now ${data.devices[i].status? "active":"not active"}`
+      });
+      return;
+    }
+    res.status(200).send({
+        "Messege":`No device found with name ${deviceName}`
+    })
+});
 
 export default router;
